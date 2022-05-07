7aeabb694df2b012e8b94565e5c55cdabda0deaa
Andre Figo <nefigo2001@gmail.com>
2022-05-07 15:57:50 +0100

    Project initial config

---
+++ src/main/java/com/scoresDei/ScoresDeiApplication.java
@@ -0,0 +1,13 @@
+package com.scoresDei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScoresDeiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScoresDeiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/scoresDei/ScoresDeiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scoresDei;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScoresDeiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
